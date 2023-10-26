d28e87602a797368a65974a86dac87b5ad98d05c
seoyoung <sy9723@korea.ac.kr>
2023-10-26 17:08:02 +0900

    feat: user entity 완료

--- src/main/kotlin/com/doubled/celenote/domain/user/User.kt
+++ src/main/kotlin/com/doubled/celenote/domain/user/User.kt
@@ -1,12 +1,33 @@
 package com.doubled.celenote.domain.user
 
+import com.doubled.celenote.domain.base.BaseEntity
 import jakarta.persistence.*
 
 @Entity
 class User(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    val id: Int = 0
+    val id: Int = 0,
 
+    @Column
+    val username: String,
 
-)
+    @Column
+    val password: String,
+
+    @Column
+    val name: String,
+
+    @Column
+    val phoneNumber: String,
+
+    @Column
+    val birthday: String,
+
+    @Column
+    val email: String,
+
+    @Column
+    val interlock: String
+
+): BaseEntity() {}
