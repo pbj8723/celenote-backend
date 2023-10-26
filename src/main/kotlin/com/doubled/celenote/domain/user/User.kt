8690a4bcdbcec1500724e6ae2ad799b7d1e9fdf5
seoyoung <sy9723@korea.ac.kr>
2023-10-26 19:21:42 +0900

    lint: format code

--- src/main/kotlin/com/doubled/celenote/domain/user/User.kt
+++ src/main/kotlin/com/doubled/celenote/domain/user/User.kt
@@ -1,37 +1,29 @@
 package com.doubled.celenote.domain.user
 
 import com.doubled.celenote.domain.base.BaseEntity
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.Column
 
 @Entity
 class User(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Int = 0,
-
     @Column
     val username: String,
-
     @Column
     val password: String,
-
     @Column
     val name: String,
-
     @Column
     val phoneNumber: String,
-
     @Column
     val birthday: String,
-
     @Column
     val email: String,
-
     @Column
     val interlock: String
-
 ) : BaseEntity()
